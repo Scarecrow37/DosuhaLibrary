70586042e16e557648861e062917e5c617041ec7
Do_Suha <weqa123@naver.com>
2025-04-23 16:42:25 +0900

    DSHInputTest 프로젝트 설정 및 테스트 추가
    DSHInputTest.vcxproj에서 `AdditionalIncludeDirectories`에 `$(SolutionDir)` 추가 및 `LanguageStandard`를 `stdcpp20`로 설정했습니다.
    SystemTest.cpp에 `DSHInput/ISystem.h`를 포함하고, 네임스페이스를 `DSH::Input`으로 변경했습니다.
    새로운 테스트 메서드 `SystemCreationSuccess`와 `SystemCreationFailure`를 추가하여 시스템 생성의 성공과 실패를 검증합니다.
    pch.h에 `DSHInput/Requires.h`를 추가하고, DSHInputTest.vcxproj에 `DSHInput` 프로젝트에 대한 참조를 설정했습니다.

--- DSHInputTest/SystemTest.cpp
+++ DSHInputTest/SystemTest.cpp
@@ -1,16 +1,32 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "DSHInput/ISystem.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace DSHInputTest
+namespace DSH::Input
 {
-	TEST_CLASS(DSHInputTest)
+	TEST_CLASS(System)
 	{
 	public:
 
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(SystemCreationSuccess)
 		{
+			CreateSystem createSystem;
+			ISystem* pSystem = nullptr;
+			HRESULT hr = createSystem(&pSystem);
+			Assert::IsTrue(SUCCEEDED(hr));
+			Assert::IsNotNull(pSystem);
+			pSystem->Release();
+		}
+
+		TEST_METHOD(SystemCreationFailure)
+		{
+			CreateSystem createSystem;
+			ISystem* pSystem = nullptr;
+			HRESULT hr = createSystem(nullptr);
+			Assert::IsTrue(FAILED(hr));
+			Assert::IsNull(pSystem);
 		}
 	};
 }

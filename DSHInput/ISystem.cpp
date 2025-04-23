033956cb4978fc66e584d1d7069424339d49cf39
Do_Suha <weqa123@naver.com>
2025-04-23 17:03:25 +0900

    메모리 할당 오류 처리 개선
    `System` 객체 생성 시 `std::nothrow`를 사용하여 메모리 할당 실패 시 `nullptr`를 반환하도록 변경하였습니다. 이를 통해 메모리 부족 상황에서의 오류 처리를 개선하였습니다.

--- DSHInput/ISystem.cpp
+++ DSHInput/ISystem.cpp
@@ -7,7 +7,7 @@ const IID DSH::Input::IID_ISystem = __uuidof(ISystem);
 HRESULT DSH::Input::CreateSystem::operator()(ISystem** ppSystem) const
 {
 	if (ppSystem == nullptr) return E_INVALIDARG;
-	System* pSystem = new System();
+	System* pSystem = new (std::nothrow) System();
 	if (pSystem == nullptr) return E_OUTOFMEMORY;
 	*ppSystem = pSystem;
 	return S_OK;
